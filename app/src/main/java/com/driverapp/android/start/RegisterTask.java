6a57e90d9be94619e17b9048664bf2b3d6b3532e
David <kioltk@gmail.com>
2015-01-04 19:11:02 +0200

    Start activity design fix and drawables inserting

---
+++ app/src/main/java/com/driverapp/android/start/RegisterTask.java
@@ -0,0 +1,38 @@
+package com.driverapp.android.start;
+
+import com.driverapp.android.core.api.ApiTask;
+import com.google.gson.Gson;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * Created by Jesus Christ. Amen.
+ */
+public abstract class RegisterTask extends ApiTask<RegisterResult> {
+
+
+    public RegisterTask(final String name, final String surname, final String source, final String profile, final String mail, final String authType, final String authToken, File imageFile) {
+        super("create_user", new ArrayList<NameValuePair>(){{
+            add(new BasicNameValuePair("name", name));
+            add(new BasicNameValuePair("surname", surname));
+            add(new BasicNameValuePair("source", source));
+            add(new BasicNameValuePair("profile", profile));
+            add(new BasicNameValuePair("mail", mail));
+            add(new BasicNameValuePair("authType", authType));
+            add(new BasicNameValuePair("authToken", authToken));
+        }},new BasicNameValuePair("avatar", imageFile.getAbsolutePath()), false);
+
+    }
+
+    @Override
+    protected RegisterResult parse(String json) {
+        return new Gson().fromJson(json, RegisterResult.class);
+    }
+}
+class RegisterResult{
+
+}
--- app/src/main/java/com/driverapp/android/start/StartActivity.java
+++ app/src/main/java/com/driverapp/android/start/StartActivity.java
@@ -1,22 +1,101 @@
 package com.driverapp.android.start;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.driverapp.android.MainActivity;
 import com.driverapp.android.R;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.VKUIHelper;
+
+import java.io.File;
+import java.io.IOException;
 
 
 public class StartActivity extends ActionBarActivity {
+
+    private static final int REQUEST_VK_AUTH = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+
+        findViewById(R.id.login_vk).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent vkAuthIntent = new Intent(getBaseContext(), );
+                VKUIHelper.onCreate(StartActivity.this);
+                VKSdk.authorize(new String[]{},true, true);
+            }
+        });
+
+        findViewById(R.id.login_force).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final View loginView = View.inflate(getBaseContext(), R.layout.dialog_login_force, null);
+                new AlertDialog.Builder(StartActivity.this).setView(loginView)
+                        .setTitle("Secret login")
+                        .setPositiveButton("Register",new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(final DialogInterface dialog, int which) {
+                                EditText nameView = (EditText) loginView.findViewById(R.id.name);
+                                EditText surnameView = (EditText) loginView.findViewById(R.id.surname);
+                                EditText sourceView = (EditText) loginView.findViewById(R.id.source);
+                                EditText profileView = (EditText) loginView.findViewById(R.id.profile);
+                                EditText mainView = (EditText) loginView.findViewById(R.id.mail);
+                                EditText authTypeView = (EditText) loginView.findViewById(R.id.auth_type);
+                                EditText authTokenView = (EditText) loginView.findViewById(R.id.auth_token);
+
+                                File imageFile = new File(getCacheDir(),"tempfile.png");
+                                try {
+                                    imageFile.createNewFile();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                                RegisterTask registerTask = new RegisterTask(
+                                        nameView.getText().toString(),
+                                        surnameView.getText().toString(),
+                                        sourceView.getText().toString(),
+                                        profileView.getText().toString(),
+                                        mainView.getText().toString(),
+                                        authTypeView.getText().toString(),
+                                        authTokenView.getText().toString(),
+                                        imageFile
+                                ) {
+                                    @Override
+                                    protected void onSuccess(RegisterResult result) {
+                                        //progressDialog.dismiss();
+                                        Toast.makeText(getBaseContext(),result.toString(),Toast.LENGTH_SHORT).show();
+                                    }
+
+                                    @Override
+                                    protected void onError(Exception exp) {
+                                        Toast.makeText(getBaseContext(),exp.toString(),Toast.LENGTH_SHORT).show();
+                                    }
+                                };
+                                registerTask.start();
+                            }
+                        })
+                        .setNegativeButton("Back", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        })
+                        .show();
+            }
+        });
+
+
         findViewById(R.id.skip).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,5 +105,9 @@ public class StartActivity extends ActionBarActivity {
         });
     }
 
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        VKUIHelper.onActivityResult(this,requestCode,resultCode,data);
+    }
 }
