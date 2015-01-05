271f512c202402b41a846a1c7fa3a32fab8a9cb1
David <kioltk@gmail.com>
2015-01-05 10:23:23 +0200

    Skipping the auth after first showing

--- app/src/main/java/com/driverapp/android/start/StartActivity.java
+++ app/src/main/java/com/driverapp/android/start/StartActivity.java
@@ -3,17 +3,17 @@ package com.driverapp.android.start;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.driverapp.android.MainActivity;
 import com.driverapp.android.R;
-import com.vk.sdk.VKSdk;
 import com.vk.sdk.VKUIHelper;
 
 import java.io.File;
@@ -29,13 +29,30 @@ public class StartActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-
+        SharedPreferences prefs = getSharedPreferences("start", MODE_MULTI_PROCESS);
+        boolean firstStart = prefs.getBoolean("first", true);
+        if(!firstStart){
+            LinearLayout container = (LinearLayout) findViewById(R.id.logins_holder);
+            container.setGravity(Gravity.CENTER);
+            findViewById(R.id.login_fb).setVisibility(View.GONE);
+            findViewById(R.id.login_vk).setVisibility(View.GONE);
+            findViewById(R.id.login_tw).setVisibility(View.GONE);
+            findViewById(R.id.login_force).setVisibility(View.GONE);
+            findViewById(R.id.skip).setVisibility(View.GONE);
+            container.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startMain();
+                }
+            },1000);
+        }
+        prefs.edit().putBoolean("first",false).apply();
         findViewById(R.id.login_vk).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Intent vkAuthIntent = new Intent(getBaseContext(), );
-                VKUIHelper.onCreate(StartActivity.this);
-                VKSdk.authorize(new String[]{},true, true);
+        //        VKUIHelper.onCreate(StartActivity.this);
+          //      VKSdk.authorize(new String[]{},true, true);
             }
         });
 
@@ -48,7 +65,7 @@ public class StartActivity extends ActionBarActivity {
                         .setPositiveButton("Register",new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(final DialogInterface dialog, int which) {
-                                EditText nameView = (EditText) loginView.findViewById(R.id.name);
+                                EditText nameView = (EditText) loginView.findViewById(R.id.user_name);
                                 EditText surnameView = (EditText) loginView.findViewById(R.id.surname);
                                 EditText sourceView = (EditText) loginView.findViewById(R.id.source);
                                 EditText profileView = (EditText) loginView.findViewById(R.id.profile);
@@ -99,10 +116,15 @@ public class StartActivity extends ActionBarActivity {
         findViewById(R.id.skip).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getBaseContext(), MainActivity.class));
-                finish();
+                startMain();
             }
         });
+    }
+
+    private void startMain() {
+
+        startActivity(new Intent(getBaseContext(), MainActivity.class));
+        finish();
     }
 
     @Override
